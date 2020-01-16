464adeedc88cd4e889da6083cd3273cd7ef35751
Jannik Becher <j.becher@depulvis.de>
2020-01-16 18:32:40 +0100

    adding supervisor

---
+++ lib/wizard/application.ex
@@ -0,0 +1,13 @@
+defmodule Wizard.Application do
+  @moduledoc false
+
+  use Application
+
+  def start(_type, _args) do
+    children = [
+      {DynamicSupervisor, strategy: :one_for_one, name: Wizard.DynamicSupervisor}
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
+  end
+end
