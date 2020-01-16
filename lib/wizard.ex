404adc4de9eec04f30b318946798fc624fa0dd0f
Jannik Becher <j.becher@depulvis.de>
2020-01-16 18:31:00 +0100

    adding public api methods

--- lib/wizard.ex
+++ lib/wizard.ex
@@ -1,18 +1,45 @@
 defmodule Wizard do
   @moduledoc """
-  Documentation for Wizard.
+  Main API for Wizard applicatoin.
   """
+  @doc """
+  Create a new Wizard game.
+  """
+  def new_game()
 
   @doc """
-  Hello world.
+  Returns the hand cards of the player.
+  """
+  def show_cards(game, player)
 
-  ## Examples
+  @doc """
+  If it's players turn: lay selected card to middle.
+  Else return error.
+  """
+  def lay_card(game, player, card)
 
-      iex> Wizard.hello()
-      :world
+  @doc """
 
   """
-  def hello do
-    :world
-  end
+  def set_trick(game, player)
+
+  @doc """
+  Returns cards in the last trick.
+  """
+  def show_last_trick(game)
+
+  @doc """
+  Returns all legal moves for the player.
+  """
+  def get_options(game, player)
+
+  @doc """
+  Returns the whole score board.
+  """
+  def get_score(game)
+
+  @doc """
+  Retruns current trump color.
+  """
+  def get_trump(game)
 end
