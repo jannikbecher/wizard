a65a3cb64458053d7d0865bea10499037a716a4c
Jannik Becher <j.becher@depulvis.de>
2020-01-16 18:08:47 +0100

    Inital commit

---
+++ lib/wizard.ex
@@ -0,0 +1,18 @@
+defmodule Wizard do
+  @moduledoc """
+  Documentation for Wizard.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Wizard.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
